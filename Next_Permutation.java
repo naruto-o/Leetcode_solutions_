143bb02eed14deef4efecc4506cf37edc61b8956
naruto <73755581+naruto-o@users.noreply.github.com>
2022-09-01 02:05:15 +0530

    Update Next_Permutation.java

--- Next_Permutation.java
+++ Next_Permutation.java
@@ -1,4 +1,48 @@
 https://leetcode.com/problems/next-permutation
+//monu bhiya solution
+class Solution {
+    public void nextPermutation(int[] nums) {
+            for(int i = nums.length-2;i>=0;i--){
+                if(nums[i]<nums[i+1]){
+                    p=i;
+                    break;
+                }
+            }
+        int q = 0;
+        for(int i = nums.length-1;i>p;i--){
+            if(nums[i]>nums[p]){
+                q=i;
+                break;
+            }
+        }
+        if(p == 0  && q == 0){
+            Range_reverse(nums,0,arr.length-1);
+            return;
+        }
+        int temp = nums[p];
+        nums[p] = nums[q];
+        nums[q] = temp;
+        Range_reverse(nums,p+1,nums.length-1);
+public static void Range_reverse(int[] arr, int i, int j) {
+
+		while (i < j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
+
+	}
+
+}
+
+        
+        
+        
+        
+        
+        
 class Solution {
     public void nextPermutation(int[] nums) {
         int mark = -1;
