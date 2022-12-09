55d8080d3a1da261751be9402c89c30529238175
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2022-12-10 00:42:22 +0530

    Update Move_zeros.java

--- Move_zeros.java
+++ Move_zeros.java
@@ -1,6 +1,21 @@
 https://leetcode.com/problems/move-zeroes/
 class Solution {
     public void moveZeroes(int[] nums) {
+        int i = 0;
+        for(int j = 0 ; j < nums.length ; j++){
+            if(nums[j] != 0) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        for(int k = i ; k < nums.length; k++){
+            nums[k] = 0;
+        }
+    }
+}
+approach 2;
+class Solution {
+    public void moveZeroes(int[] nums) {
         if(nums.length<2){
             return;
         }
