6711ea10a7eee9bf5f34f94fcb5aa90fb39df5fc
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2022-12-11 23:02:00 +0530

    Update best_time_to_buy_and_sell_stock.java

--- best_time_to_buy_and_sell_stock.java
+++ best_time_to_buy_and_sell_stock.java
@@ -1,4 +1,23 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+pseudo code explained before real java code
+{
+        int lsf = Integer.MAX_VALUE; // least so far
+        int op = 0; // overall profit
+        int pist = 0; // profit if sold today
+        
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < lsf){ // if we found new buy value which is more smaller then previous one
+                lsf = prices[i]; // update our least so far
+            }
+            pist = prices[i] - lsf; // calculating profit if sold today by, Buy - sell
+            if(op < pist){ // if pist is more then our previous overall profit
+                op = pist; // update overall profit
+            }
+        }
+        return op; // return op 
+     }
+ }
+     
 class Solution {
     public int maxProfit(int[] prices) {
        int  min_val = Integer.MAX_VALUE;
