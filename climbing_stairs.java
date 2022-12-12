9ad4304609e0ff5d0711979b1942a10f3d4ca6bc
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2022-12-12 22:44:09 +0530

    Update climbing_stairs.java

--- climbing_stairs.java
+++ climbing_stairs.java
@@ -17,3 +17,25 @@ class Solution {
         return dp[n];
     }        
 }
+public class ClimbingStairs_Iterative {
+
+    public int climbStairs(int n) {
+
+        // Return the ways for n == 1 and n == 2, since we know the result already.
+        if (n == 1 || n == 2) return n;
+
+        // When using tabulation, seed the table with the base cases for n == 1 and n == 2.
+        // Since the array in Java is zero-indexed, we make sure to have the table of size "n + 1"
+        // as we need to access index 'n'.
+        int[] table = new int[n + 1];
+        table[1] = 1;
+        table[2] = 2;
+        // Iterate through the table and update its values until 'n'.
+        for (int i = 3; i <= n; i++) {
+            table[i] = table[i - 1] + table[i - 2];
+        }
+
+        // table[n] is the result.
+        return table[n];
+    }
+}
