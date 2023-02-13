ebc0b98a1100591bd5639ce01de1971d8bd4c52b
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2023-02-14 01:52:09 +0530

    Update Majority_element.java

--- Majority_element.java
+++ Majority_element.java
@@ -23,3 +23,22 @@ class Solution {
         return 0;
     }
 }
+// moore voting algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count =0;
+        int element = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(count == 0){
+                element = nums[i];
+            }
+            if(nums[i] == element){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+    return element;
+    }
+}
