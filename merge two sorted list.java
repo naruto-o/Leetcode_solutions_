a077a5f68c87981fff1b0a131a07e3cea102d406
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2023-03-29 14:36:38 +0530

    Update merge two sorted list.java

--- merge two sorted list.java
+++ merge two sorted list.java
@@ -10,6 +10,35 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+           ListNode dummyhead = new ListNode();
+          ListNode head = dummyhead;
+        while(list1 != null && list2!=null){
+            if(list1.val < list2.val){
+                head.next = list1;
+                list1 = list1.next;
+                head  = head.next;
+            }
+            else{
+                head.next = list2;
+                list2 = list2.next;
+                head  = head.next;
+            }
+        }
+        while(list1 != null){
+            head.next = list1;
+            list1 = list1.next;
+            head = head.next;
+        }
+        while(list2 != null){
+            head.next = list2;
+            list2 = list2.next;
+            head = head.next;
+        }
+        return dummyhead.next;
+    }
+}
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
         if(list1==null) return list2;
         else if(list2==null) return list1;
