c26fa7b3690ce3f8a0bd592d8cd032c2bb3d1d12
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2025-01-10 19:50:34 +0530

    Create Rotate Image.java

---
+++ Rotate Image.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int arr[][] = new int[n][n];
+        for(int i = 0 ; i<matrix[0].length;i++){
+            for(int j = 0;j<matrix.length;j++){
+                arr[j][n-1-i] = matrix[i][j];
+            }
+        }
+        for(int i = 0 ; i<matrix.length;i++){
+            for(int j = 0;j<matrix[0].length;j++){
+                matrix[i][j]=arr[i][j];
+            }
+        }
+    }
+}
