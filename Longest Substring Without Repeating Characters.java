36a2cc59c84c63f10ccd3376b44bc28ed9b75ae2
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2026-09-23 14:38:58 +0530

    Update Longest Substring Without Repeating Characters.java
    Updated the logic to use a sliding window approach with a hashmap.

--- Longest Substring Without Repeating Characters.java
+++ Longest Substring Without Repeating Characters.java
@@ -1,45 +1,19 @@
 https://leetcode.com/problems/longest-substring-without-repeating-characters/
 class Solution {
-    public int lengthOfLongestSubstring(String str) {
-        int ans = 0;
-        int i = -1;
-        int j = -1;
-        HashMap<Character,Integer> map = new HashMap<>();
-        while(true){
-            boolean flag1 = false;
-            boolean flag2 = false;
-            //acquire
-            while(i<str.length() -1){
-                flag1 = true;
-                i++;
-                char ch = str.charAt(i);
-                map.put(ch,map.getOrDefault(ch,0)+1);
-                
-                if(map.get(ch) == 2){
-                    break;
-                }
-                else{
-                    int len = i-j;
-                    if(len>ans){
-                        ans  = len;
-                    }
-                }
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character,Integer>map = new HashMap<>();
+        int low = 0;
+        int res =0;
+        for(int high = 0;high<s.length();high++){
+            char ch = s.charAt(high);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            while(map.get(ch)>1){
+                char lch = s.charAt(low);
+                map.put(lch,map.get(lch)-1);
+                low++;
             }
-            //release
-            while(j<i){
-                flag2=true;
-                j++;
-                char ch = str.charAt(j);
-                map.put(ch,map.get(ch)-1);
-                
-                if(map.get(ch) == 1){
-                    break;
-                }
-            }
-            if(flag1==false && flag2==false){
-                break;
-            }
+            res = Math.max(res,high-low+1);
         }
-        return ans;
+        return res;
     }
 }
